83379f04cd5491569cfdff8ed5f86518aa706355
224389 Olaszczyk Oskar <224389@edu.p.lodz.pl>
2020-11-28 21:32:13 +0100

    fixed search engine

--- src/components/SeachEnigne.vue
+++ src/components/SeachEnigne.vue
@@ -2,29 +2,28 @@
 <form>
         <div>
             <label for=inputTitle>Tytuł</label>
-            <input type="text" id=inputTitle placeholder="Podaj tytuł lub fragment tytułu filmu"/>
+            <input v-model="inputTitle" type="text" id=inputTitle placeholder="Podaj tytuł lub fragment tytułu movieu"/>
         </div>
         <div>
           <label for="inputProductionFrom">Rok produkcji od:</label>
           <div>
-              <input type="text" id=inputProductionFrom placeholder="Liczba naturalna z przedziału 1900-2019"/>
+              <input v-model="inputProductionFrom" type="text" id=inputProductionFrom placeholder="Liczba naturalna z przedziału 1900-2019"/>
           </div>
         </div>
         <div>
             <label for="inputProductionTo">Rok produkcji do:</label>
             <div>
-                <input type="text" id=inputProductionTo placeholder="Liczba naturalna z przedziału 1900-2019"/>
+                <input v-model="inputProductionTo" type="text" id=inputProductionTo placeholder="Liczba naturalna z przedziału 1900-2019"/>
             </div>
         </div>
         <div>
           <label for="inputCast">Obsada</label>
-          <input type="text" id="inputCast" placeholder="Imię i nazwisko"/>
+          <input v-model="inputCast" type="text" id="inputCast" placeholder="Imię i nazwisko"/>
         </div>
         <div>
             <button
           type="button"
-          v-on:click="search"
-      >
+          v-on:click="search">
         Search
       </button>
         </div>
@@ -33,7 +32,7 @@
 
 <script>
       import movies from '../assets/movies.json';
-        import _ from 'underscore'
+      import _ from 'lodash'
 
       export default {
             name: "SearchEngine",
@@ -41,35 +40,35 @@
                   return {
                         movies: movies,
                         inputTitle: "",
-      inputYearFrom: "",
-      inputYearTo: "",
-      inputCast: "",
-      listEmitted: movies,
+                        inputProductionFrom: "",
+                        inputProductionTo: "",
+                        inputCast: "",
+                        listEmitted: movies,
                   }
             },
-            methods: {
-                search: function () {
-                let self = this;
-                this.listEmitted = _.filter(this.movies, function (film) {
-                    if (
-                        (self.inputTitle === "" ||
-                            film.title.toLowerCase().includes(self.inputTitle.toLowerCase())) &&
-                        (film.year >= self.inputYearFrom || self.inputYearFrom === "") &&
-                        (film.year <= self.inputYearTo || self.inputYearTo === "")
-                    ) {
-                    if (self.inputCast === "") {
-                        return true;
-                    } else {
-                        for (let i = 0; i < film.cast.length; i++) {
-                        if (film.cast[i].toLowerCase() === self.inputCast.toLowerCase()) {
-                            return true;
-                        }
-                        }
-                    }
-                    }
-                    return false;
-                });
-                this.$emit("search-event", self.listEmitted);
+             methods: {
+    search: function () {
+      let self = this;
+      this.listEmitted = _.filter(this.movies, function (film) {
+        if (
+            (self.inputTitle === "" ||
+                film.title.toLowerCase().includes(self.inputTitle.toLowerCase())) &&
+            (film.year >= self.inputProductionFrom || self.inputProductionFrom === "") &&
+            (film.year <= self.inputProductionTo|| self.inputProductionTo=== "")
+        ) {
+          if (self.inputCast === "") {
+            return true;
+          } else {
+            for (let i = 0; i < film.cast.length; i++) {
+              if (film.cast[i].toLowerCase() === self.inputCast.toLowerCase()) {
+                return true;
+              }
+            }
+          }
+        }
+        return false;
+      });
+      this.$emit("search-event", self.listEmitted);
     },
   },
 };
